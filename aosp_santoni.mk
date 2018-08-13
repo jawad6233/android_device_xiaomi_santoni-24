e9d759f64b7cdda6b92307a08ec6bef4ab6282d1
Sonal Singh Sisodiya <sonal.singh.19993@gmail.com>
2018-08-13 18:34:16 +0000

    Add Device Maintainer

--- aosp_santoni.mk
+++ aosp_santoni.mk
@@ -28,6 +28,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="santoni-user 7.1.2 N2G47H V9.5.4.0.NAMMIFA release-keys"
+    DEVICE_MAINTAINERS="Sonal Singh"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "Xiaomi/santoni/santoni:7.1.2/N2G47H/V9.5.4.0.NAMMIFA:user/release-keys"
