17de577b7e302cf6025b338c4611f7341de28e67
Sonal Singh <sonal.singh.19993@gmail.com>
2018-10-08 17:45:38 +0000

    santoni: Fix Bluetooth Calling

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -16,7 +16,7 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-#define BTM_DEF_LOCAL_NAME   "Xiaomi Redmi 4X"
+#define BTM_DEF_LOCAL_NAME   "Xiaomi Redmi 4"
 
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
@@ -24,8 +24,8 @@
 #define BLE_VND_INCLUDED   TRUE
 
 // skips conn update at conn completion
-#define BT_CLEAN_TURN_ON_DISABLED 1
-#define BTM_SCO_ENHANCED_SYNC_DISABLED TRUE
+#define BT_CLEAN_TURN_ON_DISABLED TRUE
+#define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
 
 #define BTM_WBS_INCLUDED TRUE
 #define BTIF_HF_WBS_PREFERRED TRUE
